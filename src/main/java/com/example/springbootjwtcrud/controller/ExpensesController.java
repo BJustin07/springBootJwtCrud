bd61cdf617fe029a68472bdead227b138974d6f4
BJustin07 <baquiranjustin07@gmail.com>
2025-09-08 21:02:09 +0800

    init

---
+++ src/main/java/com/example/springbootjwtcrud/controller/ExpensesController.java
@@ -0,0 +1,4 @@
+package com.example.springbootjwtcrud.controller;
+
+public class ExpensesController {
+}
---
+++ src/test/java/com/example/springbootjwtcrud/SpringBootJwtCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwtcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
